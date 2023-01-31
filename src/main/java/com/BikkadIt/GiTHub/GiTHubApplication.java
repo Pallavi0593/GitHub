19f0e1c743336a8d7bb03acd0dab1f190717f0b7
Pallavi0593 <pallavi.dhatkar@gmail.com>
2023-01-31 22:33:40 +0530

    changes by pallavi

---
+++ src/main/java/com/BikkadIt/GiTHub/GiTHubApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIt.GiTHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiTHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiTHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BikkadIt/GiTHub/GiTHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikkadIt.GiTHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiTHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
